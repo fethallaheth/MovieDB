3f2d49587f76780c873141ac892f87208b26fe8c
Unnamed <109878322+fethallaheth@users.noreply.github.com>
2023-07-24 02:49:25 +0100

    Update SubmitButton.tsx

--- app/components/SubmitButton.tsx
+++ app/components/SubmitButton.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-import { experimental_useFormStatus as experimental_useFormStatus} from "react-dom";
-
 export default function SubmitButton() {
-  const { pending } = experimental_useFormStatus();
   return (
     <button
       type="submit"
-      disabled={pending}
       className="bg-teal-500 px-4 py-2 rounded-lg text-white"
     >
-      {pending ? <>loading...</> : <>Add comment...</>}
+      {<>pending</> ? <>loading...</> : <>Add comment...</>}
     </button>
   );
 }
